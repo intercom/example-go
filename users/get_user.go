c12966171783fd69d3fab091bd8255e2f71f9b86
Cathal Horan <cathal.horan@intercom.io>
2017-02-10 05:48:56 -0500

    Changing PAT to AT for env variable. Can use either when running docker cmd but matching name change from personal access token to access token

--- users/get_user.go
+++ users/get_user.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
     user_id := os.Args[1]
-    ic := intercom.NewClient(os.Getenv("PAT"), "")
+    ic := intercom.NewClient(os.Getenv("AT"), "")
     user, err := ic.Users.FindByUserID(user_id)
     fmt.Printf("INTERCOM GO \n")
     fmt.Printf("%s \n", user.Email)
